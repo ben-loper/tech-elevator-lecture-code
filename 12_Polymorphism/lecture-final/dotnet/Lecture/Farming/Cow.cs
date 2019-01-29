3bb14020af9dae87d2d55388d4a90311fdc2c30e
Chris Rupp <chris@techelevator.com>
2019-01-29 11:10:34 -0500

    polymorphism

---
+++ 12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Cow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Farming
+{
+    public class Cow : FarmAnimal
+    {
+        public Cow() : base("Cow", 5000.00M)
+        {
+            
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "Moo";
+        }
+
+        public string Graze()
+        {
+            return "Hey I love to eat grass because I'm a COW!!!!";
+        }
+
+    }
+}
---
+++ 12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Duck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Farming
+{
+    public class Duck : FarmAnimal
+    {
+        public Duck() : base("Duck", 5.00M)
+        {
+            
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "Quack";
+        }
+    }
+}
