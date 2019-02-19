3d47be52199f90865967627e55116a425440a8cc
Chris Rupp <chris@techelevator.com>
2019-02-19 12:17:27 -0500

    integration testing

--- module 2/07_Integration_Testing/lecture-final/dotnet/WorldGeography.Tests/DAL/LanguageSqlDALTests.cs
+++ module 2/07_Integration_Testing/lecture-final/dotnet/WorldGeography.Tests/DAL/LanguageSqlDALTests.cs
@@ -15,7 +15,7 @@ namespace WorldGeography.DAL.Tests
     public class LanguageSqlDALTests
     {
         private TransactionScope tran;
-        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=World;User ID=te_student;Password=sqlserver1";
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=World;Integrated Security=true";
 
 
         /*
