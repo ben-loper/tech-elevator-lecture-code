6e9bd3db32c4453cbc112b26650f2bb213f95186
Chris Rupp <chris@techelevator.com>
2019-02-27 12:06:30 -0500

    views

---
+++ module 3/03-Introduction-to-MVC-Views/lecture-final/dotnet/TechElevator.Web/Models/MathTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechElevator.Web.Models
+{
+    public class MathTime
+    {
+        public static int SumMyNums(int[] values)
+        {
+            int result = 0;
+            foreach(var value in values)
+            {
+                result += value;
+            }
+            return result;
+        }
+    }
+}
