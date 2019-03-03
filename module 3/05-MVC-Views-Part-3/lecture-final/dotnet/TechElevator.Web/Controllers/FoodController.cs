41e37cd001a416b9c3076dcbb0705d086f05a2fe
Chris Rupp <chris@techelevator.com>
2019-03-03 16:45:46 -0500

    forms with get

---
+++ module 3/05-MVC-Views-Part-3/lecture-final/dotnet/TechElevator.Web/Controllers/FoodController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechElevator.Web.DAL;
+using TechElevator.Web.Models;
+
+namespace TechElevator.Web.Controllers
+{
+    public class FoodController : Controller
+    {
+        [HttpGet]
+        // /food (works because default action is Index)
+        // /     (works because default controller and action are set to Food and Index)
+        // /food/index
+        public IActionResult Index()
+        {
+            List<CreatureViewModel> data = new List<CreatureViewModel>();
+            CreatureViewModel creature = new CreatureViewModel();
+            creature.Name = "Ewok";
+            creature.ImageUrl = "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/5/24/13/enhanced-buzz-28303-1337880144-7.jpg?downsize=700:*&output-format=auto&output-quality=auto";
+            creature.ImageSize = "35%";
+            data.Add(creature);
+
+            creature = new CreatureViewModel();
+            creature.Name = "Wookie";
+            creature.ImageUrl = "https://contattafiles.s3.us-west-1.amazonaws.com/tecommunity/6oNYJw54EECOLse/Pasted%20Image%3A%20Mar%201%2C%202019%20-%2011%3A15%3A09am";
+            creature.ImageSize = "70%";
+            data.Add(creature);
+
+            creature = new CreatureViewModel();
+            creature.Name = "Jawa";
+            creature.ImageUrl = "https://i.imgur.com/sT9bTkIl.jpg";
+            creature.ImageSize = "100%";
+            data.Add(creature);
+
+            return View(data);
+        }
+
+        [HttpGet]
+        //[Route("food/breakfast")]
+        // /food/creaturebreakfast
+        public IActionResult CreatureBreakfast(string creature)
+        {
+            // Received request from browers (Step 1 of MVC workflow)
+
+            if (creature == null)
+            {
+                return NotFound();
+            }
+
+            IStarWarsDAL dal = new StarWarsDAL();
+            EatDetailViewModel vm = new EatDetailViewModel();
+            vm.Creature = creature;
+            vm.Meal = "breakfast";
+
+            // Get data from Model (Step 2 & 3 of MVC workflow)
+            vm.FoodItems = dal.GetFoodInfo(vm.Creature, vm.Meal);
+
+            // Send data to View (Step 4 & 5 of MVC workflow)
+            var htmlResult = View("Eat", vm);
+
+            // Send View data to browser (Step 6 of MVC workflow)
+            return htmlResult;
+        }
+        
+        [HttpGet]
+        // /food/eat?creature=wookie&meal=breakfast
+        // /food/eat/wookie/breakfast
+        public IActionResult Eat(EatViewModel eatInfo)
+        {
+            if (eatInfo.Creature == null || eatInfo.Meal == null)
+            {
+                return NotFound();
+            }
+
+            // Received request from browers (Step 1 of MVC workflow)
+
+            IStarWarsDAL dal = new StarWarsDAL();
+
+            EatDetailViewModel vm = new EatDetailViewModel();
+            vm.Creature = eatInfo.Creature;
+            vm.Meal = eatInfo.Meal;
+
+            // Get data from Model (Step 2 & 3 of MVC workflow)
+            vm.FoodItems = dal.GetFoodInfo(eatInfo.Creature, eatInfo.Meal);
+
+            // Send data to View (Step 4 & 5 of MVC workflow)
+            var htmlResult = View(vm);
+
+            // Send View data to browser (Step 6 of MVC workflow)
+            return htmlResult;
+        }
+
+        //[HttpGet]
+        //public IActionResult Eat(EatViewModel eatInfo)
+        //{
+        //    EatDetailViewModel vm = new EatDetailViewModel();
+        //    vm.Creature = eatInfo.Creature;
+        //    vm.Meal = eatInfo.Meal;
+
+        //    List<string> test = new List<string>()
+        //    {
+        //        "banana",
+        //        "salmon",
+        //        "John"
+        //    };
+        //    vm.FoodItems = test;
+
+        //    ViewData["EatInfo"] = vm;
+
+        //    return View();
+        //}
+    }
+}
---
+++ module 3/05-MVC-Views-Part-3/lecture-final/dotnet/TechElevator.Web/DAL/IStarWarsDAL.cs
@@ -0,0 +1,32 @@
+﻿using TechElevator.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechElevator.Web.DAL
+{
+    public interface IStarWarsDAL
+    {
+        /// <summary>
+        /// Returns all of the films.
+        /// </summary>
+        /// <returns></returns>
+        IList<Film> GetFilms();
+
+        /// <summary>
+        /// Returns a single film
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Film GetFilm(string id);
+
+        /// <summary>
+        /// Gets the food a Star Wars creature would eat for a given meal
+        /// </summary>
+        /// <param name="creature">The creature in question (Wookie, Jawa, Ewok)</param>
+        /// <param name="meal">The meal to be eaten (Breakfast, Lunch, Dinner)</param>
+        /// <returns>List of food items consumed at meal</returns>
+        List<string> GetFoodInfo(string creature, string meal);
+    }
+}
