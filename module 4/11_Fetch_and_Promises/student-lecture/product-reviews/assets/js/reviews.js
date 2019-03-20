9e6f815f2345d115615f0e17a8f59374ff7a851b
Chris Rupp <chris@techelevator.com>
2019-03-20 12:56:31 -0400

    final lecture

--- module 4/11_Fetch_and_Promises/student-lecture/product-reviews/assets/js/reviews.js
+++ module 4/11_Fetch_and_Promises/student-lecture/product-reviews/assets/js/reviews.js
@@ -2,11 +2,31 @@ function loadReviews() {
     console.log("Load Reviews...");
   
     fetch('data.json') 
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        reviews = data;
-      })
-      .catch((err) => console.error(err));
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      reviews = data;
+    })
+    .catch((err) => console.error(err));
   }
+
+
+//   fetch(ajaxURL, {
+//     method: 'post', // *GET, POST, PUT, DELETE, etc.
+//     mode: 'cors', // no-cors, cors, *same-origin
+//     headers: {
+//         "Content-Type": "application/json",
+//         // "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     redirect: "follow", // manual, *follow, error
+//     referrer: "no-referrer", // no-referrer, *client
+//     body: JSON.stringify(player),
+// })
+// .then(response => response.json())
+// .then( (data) => {     // this is a bit of magic for now, just know that response is a Response object
+//         console.log(data.message);  // this block is where we write code to handle the HTTP response, here we're just logging the response object
+// })
+// .catch(error => console.error('Error:', error));
