e1c85e25e814501fecfc4506e20e2f377d5b4dfd
Ben Loper <loperb06@gmail.com>
2019-03-27 07:56:08 -0400

    Merge

--- module 4/11_Fetch_and_Promises/student-tutorial/todo-fetch/assets/js/todo.js
+++ module 4/11_Fetch_and_Promises/student-tutorial/todo-fetch/assets/js/todo.js
@@ -1,2 +1,30 @@
 
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch('data.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonObjects) => {
+        const todoList = document.querySelector('ul');
+
+        jsonObjects.forEach(jsonObject => {
+            const liNode = document.createElement('li');
+            liNode.innerText = jsonObject[`task`];
+            
+            checkMarkNode = document.createElement('i');
+            checkMarkNode.classList.add('far');
+            checkMarkNode.classList.add('fa-check-circle');
+
+            liNode.insertAdjacentElement('beforeend', checkMarkNode);
+
+            if(jsonObject[`completed`]){
+                liNode.classList.add(`todo-completed`);
+                checkMarkNode.classList.add('completed');
+            }
+
+            todoList.insertAdjacentElement('beforeend', liNode);
+        });
+    })
+});
